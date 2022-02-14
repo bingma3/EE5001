7bb08fe1d9ee2253c2e2babd69c7b3b70afc55db
bingma3 <79664191+bingma3@users.noreply.github.com>
2022-02-14 22:14:45 +0000

    unittest for assignment 1

---
+++ assignment1_unittest.py
@@ -0,0 +1,51 @@
+import unittest
+from AES_128bit_rework import ASE_128
+from present_80 import PRESENT_80
+
+
+class MyTestCase(unittest.TestCase):
+    def test_aes_encrypt(self):
+        test_plaintext = ['0123456789abcdeffedcba9876543210',
+                          '6bc1bee22e409f96e93d7e117393172a',
+                          '00112233445566778899aabbccddeeff',
+                          '000102030405060708090a0b0c0d0e0f',
+                          '00000000000000000000000000000000',
+                          '00000000000000000000000000000000']
+        test_key = ['0f1571c947d9e8590cb7add6af7f6798',
+                    '2b7e151628aed2a6abf7158809cf4f3c',
+                    '000102030405060708090a0b0c0d0e0f',
+                    '000102030405060708090a0b0c0d0e0f',
+                    '00000000000000000000000000000000',
+                    'ffffffffffffffffffffffffffffffff']
+        test_ciphertext = ['ff0b844a0853bf7c6934ab4364148fb9',
+                           '3ad77bb40d7a3660a89ecaf32466ef97',
+                           '69c4e0d86a7b0430d8cdb78070b4c55a',
+                           '0a940bb5416ef045f1c39458c653ea5a',
+                           '66e94bd4ef8a2c3b884cfa59ca342b2e',
+                           'a1f6258c877d5fcd8964484538bfc92c']
+        aes = ASE_128()
+        for _ in range(100):
+            for i in range(len(test_plaintext)):
+                self.assertEqual(aes.encrypt(test_plaintext[i], test_key[i]), test_ciphertext[i])
+
+    def test_present_encrypt(self):
+        test_plaintext = ['0000000000000000',
+                          '0000000000000000',
+                          'FFFFFFFFFFFFFFFF',
+                          'FFFFFFFFFFFFFFFF']
+        test_key = ['0000000000000000',
+                    'FFFFFFFFFFFFFFFFFFFF',
+                    '0000000000000000',
+                    'FFFFFFFFFFFFFFFFFFFF']
+        test_ciphertext = ['5579C1387B228445',
+                           'E72C46C0F5945049',
+                           'A112FFC72F68417B',
+                           '3333DCD3213210D2']
+        present = PRESENT_80()
+        for _ in range(100):
+            for i in range(len(test_plaintext)):
+                self.assertEqual(present.encrypt(test_plaintext[i], test_key[i]).upper(), test_ciphertext[i])
+
+
+if __name__ == '__main__':
+    unittest.main()
--- present_80.py
+++ present_80.py
@@ -13,6 +13,14 @@ class PRESENT_80:
 
     # create a round-key array for each round
     def generate_round_key(self, k):
+        """
+        :param k: Original Key
+        :return: Generate 32 round keys follows the pattern below
+            1. At round i the 64-bit round key consists of the 64 leftmost bits of the current contents of key.
+            2. Rotated 61 bits to the left
+            3. Pass the left-most four bits through the present S-box
+            4. round number i XOR with bits 19,18,17,16,15 of key
+        """
         round_key = []
         for i in range(1, self.ROUND + 1):
             # At round i the 64-bit round key consists of the 64 leftmost bits of the current contents of key.
@@ -28,30 +36,65 @@ class PRESENT_80:
 
     @staticmethod
     def add_round_key(s, k):
+        """
+        :param s: round state bit
+        :param k: round key bit
+        :return: s XOR k
+        """
         return s ^ k
 
     def s_box_layer(self, text):
+        """
+        :param text: a hexadecimal number represent 4 bit binary number
+        :return: notation to the hexadecimal number regards to the given Sbox table index number.
+        """
         tmp_text = 0
         for i in range(16):
             tmp_text += self.Sbox[(text >> (i * 4) & 0xf)] << (i * 4)
         return tmp_text
 
     def p_layer(self, text):
+        """
+        :param text: the leftmost 64 of round state
+        :return: Bit i of state is moved to bit position of Pbox(i)
+        """
         tmp_text = 0
         for i in range(64):
             tmp_text += ((text >> i) & 0x01) << self.Pbox[i]
         return tmp_text
 
-    def encryption(self, text, k):
-        round_key = self.generate_round_key(k)
-        state_text = text
+    @staticmethod
+    def int2hex(i):
+        return hex(i)[2:].zfill(16)
+
+    @staticmethod
+    def hex2int(h):
+        return int.from_bytes(bytes.fromhex(h), byteorder='big')
+
+    def encrypt(self, text, k):
+        """
+        :param text: Original plaintext
+        :param k: Original Key
+        :return: Ciphertext
+        run the pattern below of 32 - 1 rounds:
+            1. add the round key
+            2. notate the 16 4-bit state number as Sbox table
+            3. run the permutation
+         *the last round add the round key only
+        """
+        round_key = self.generate_round_key(self.hex2int(k))
+        state_text = self.hex2int(text)
         for i in range(self.ROUND-1):
+            # add the round key
             state_text = self.add_round_key(state_text, round_key[i])
+            # SBox layer
             state_text = self.s_box_layer(state_text)
+            # PBox layer
             state_text = self.p_layer(state_text)
             # print(f"Round {i+1} output: {'0x'+ hex(state_text)[2:].zfill(16)}")
-        state_text = self.add_round_key(state_text, round_key[-1])
-        return state_text
+        # the last round of state
+        output = self.add_round_key(state_text, round_key[-1])
+        return self.int2hex(output)
 
 
 if __name__ == '__main__':
@@ -64,16 +107,12 @@ if __name__ == '__main__':
     FFFFFFFF FFFFFFFF | FFFFFFFF FFFFFFFF FFFF | 3333DCD3 213210D2
     '''
     plaintext = "0000000000000000"
-    plaintext = bytes.fromhex(plaintext)
-    plaintext = int.from_bytes(plaintext, byteorder='big')
     key = "FFFFFFFFFFFFFFFFFFFF"
-    key = bytes.fromhex(key)
-    key = int.from_bytes(key, byteorder='big')
     start_time = time.perf_counter()
     present = PRESENT_80()
-    ciphertext = present.encryption(plaintext, key)
+    ciphertext = present.encrypt(plaintext, key)
     end_time = time.perf_counter()
-    print(f"Plaintext: {'0x' + hex(plaintext)[2:].zfill(16)}")
-    print(f"Key: {'0x' + hex(key)[2:].zfill(20)}")
-    print(f"Ciphertext: {'0x'+ hex(ciphertext)[2:].zfill(16)}")
+    print(f"Plaintext: {plaintext}")
+    print(f"Key: {key}")
+    print(f"Ciphertext: {ciphertext.upper()}")
     print(f"Time used: {end_time-start_time}")
